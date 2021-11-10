13e3db088373ff04d8951ee6a7942966304809fe
JulianVentura <julianv6004@gmail.com>
2021-11-10 18:44:42 -0300

    Agrego conexion de prueba entre webservice y AlGlobo

--- alglobo/src/main.rs
+++ alglobo/src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpStream;
+
+
+fn run() {
+    let direccion = "localhost:6000".to_string();
+    let mut resultado = String::new();
+    let monto = String::from("123.56") + "\n";
+    let mut web_service = TcpStream::connect(direccion).unwrap();
+    web_service.write(monto.as_bytes()).unwrap();
+    let mut reader = BufReader::new( &web_service);
+    reader.read_line(&mut resultado).unwrap();
+    
+    println!("{}", resultado);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    run()
 }
--- webservices/src/main.rs
+++ webservices/src/main.rs
@@ -1,3 +1,38 @@
+use std::{io::{BufRead, BufReader, Write}, net::TcpListener};
+use rand::Rng;
+use std::{thread, time};
+
+fn simulate_work() -> Result<(), ()> {
+    let mut rng = rand::thread_rng();
+    let tiempo_trabajo = rng.gen_range(300..1000);
+    let ok = rng.gen::<f32>() >= 0.2;
+    thread::sleep(time::Duration::from_millis(tiempo_trabajo));
+
+    if ok { Ok(()) } else { Err(()) }
+}
+
+
+fn run() {
+    let direccion = "localhost:6000".to_string();
+    let listener = TcpListener::bind(direccion).unwrap();
+    
+    for stream in listener.incoming() {
+        let mut numero = String::new();
+        if let Ok(mut stream) = stream {
+            let mut reader = BufReader::new( &stream);
+            reader.read_line(&mut numero).unwrap();
+            let monto = numero.replace("\n", "").parse::<f64>().unwrap();
+            println!("Procesando monto: {}", monto);
+            let resultado = match simulate_work() {
+                Ok(_) => "OK",
+                Err(_) => "ERROR"
+            };
+            stream.write(resultado.as_bytes()).unwrap();
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+   run()
 }
