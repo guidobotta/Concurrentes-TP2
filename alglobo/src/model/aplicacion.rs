5c58bc6d2ea5cf2e6698d9294fd9385919440e35
JulianVentura <julianv6004@gmail.com>
2021-12-07 18:03:56 -0300

    Se modifica logica de Aplicacion

--- alglobo/src/model/aplicacion.rs
+++ alglobo/src/model/aplicacion.rs
@@ -44,52 +44,54 @@ impl Aplicacion {
         mut escritor: EscritorFallidos,
         continuar: Arc<AtomicBool>,
     ) {
+    
+        while continuar.load(Ordering::Relaxed) {
+            if lider.am_i_leader() {
+                Aplicacion::procesar_lider(&lider, &mut parseador, id);
+            } else { 
+                //No somos el lider, ver que hacer para detectar caida de lider (No hacer busy waiting)
+            }
+        }
+    }
+    
+    fn procesar_lider(lider: &LeaderElection, parseador: &mut Parser, id: usize) {
         let mut log = Log::new("./files/estado.log".to_string()).unwrap();
         let mut coordinador = CoordinadorTransaccion::new(id, log.clone());
         let mut inicio_lider = true;
         let mut transaccion;
         let mut prox_pago = 0;
 
-        while continuar.load(Ordering::Relaxed) {
-            if lider.am_i_leader() {
-                if inicio_lider {
-                    transaccion = Transaccion::default(0);
-                    //Aca obtenemos la ultima transaccion que puede o no ser un reintento
-                    //transaccion = log.ultima_transaccion();
-                    //if transaccion.es_reintento() {
-                    //    transaccion.pago = parseador.parsear_fallidos(transaccion.id_pago).unwrap();
-                    //    prox_pago = transaccion.id_pago_prox
-                    //} else {
-                    //    transaccion.pago = parseador.parsear_nuevo(Some(transaccion.id_pago)).unwrap();
-                    //}
-                    //let prox_pago = transaccion.id_pago_prox;
-                    inicio_lider = false;
-                } else if false { // !cola_reintentos.empty? { //Reintento, mensaje por socket.
-                    //pago = socket.reintento();
-                    //transaccion = log.nueva_transaccion(prox_pago);
-                    //transaccion.id_pago = pago.id;
-                    //transaccion.pago = parseador.parsear_fallidos(pago.id);
-                    transaccion = Transaccion::default(0);
-                } else {
-                    transaccion = log.nueva_transaccion(prox_pago);
-                    transaccion.pago = parseador.parsear_nuevo(None).unwrap()
-                }
-                //Procesar transaccion
-                if coordinador.submit(&mut transaccion).is_err() {
-                    //Agregar a la lista de falladas
-                    println!("El pago de id {} ha fallado", &transaccion.id_pago);
-                    transaccion.get_pago()
-                    .and_then(|p| Some(escritor.escribir_fallido(p)));
-                }
-            } else { //No somos el lider
-                inicio_lider = true;
+        while lider.am_i_leader() {
+            //Este if inicio_lider se puede sacar fuera del while, porque ya sabemos que es lider
+            if inicio_lider {
+                transaccion = Transaccion::default(0);
+                //Aca obtenemos la ultima transaccion que puede o no ser un reintento
+                //transaccion = log.ultima_transaccion();
+                //if transaccion.es_reintento() {
+                //    transaccion.pago = parseador.parsear_fallidos(transaccion.id_pago).unwrap();
+                //    prox_pago = transaccion.id_pago_prox
+                //} else {
+                //    transaccion.pago = parseador.parsear_nuevo(Some(transaccion.id_pago)).unwrap();
+                //}
+                //let prox_pago = transaccion.id_pago_prox;
+                inicio_lider = false;
+            } else if false { // !cola_reintentos.empty? { //Reintento, mensaje por socket.
+                //pago = socket.reintento();
+                //transaccion = log.nueva_transaccion(prox_pago);
+                //transaccion.id_pago = pago.id;
+                //transaccion.pago = parseador.parsear_fallidos(pago.id);
+                transaccion = Transaccion::default(0);
+            } else {
+                transaccion = log.nueva_transaccion(prox_pago);
+                transaccion.pago = parseador.parsear_nuevo(None).unwrap()
+            }
+            //Procesar transaccion
+            if coordinador.submit(&mut transaccion).is_err() {
+                //Agregar a la lista de falladas
+                println!("El pago de id {} ha fallado", &transaccion.id_pago);
+                transaccion.get_pago()
+                .and_then(|p| Some(escritor.escribir_fallido(p)));
             }
         }
-    }
-
-    fn procesar_pago(_pago: &Pago) -> Resultado<()> {
-        //Procesar transaccionalidad a los webservices
-        
-        Ok(())
     }
 }
