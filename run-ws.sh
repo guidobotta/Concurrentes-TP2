f75e0162166a83a2eaa264e49c2558e0a49e4f3c
guidobotta <gbotta@fi.uba.ar>
2021-12-09 01:45:28 -0300

    D

--- run-ws.sh
+++ run-ws.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]
     exit
 fi
 
-if [ $2 -eq "-r" ]
+if [ $2 = "-r" ]
   then
     rm alglobo/files/fallidos.csv
     rm alglobo/files/estado.log
@@ -18,11 +18,11 @@ chmod +x 2-webservices.sh
 for j in `seq 0 2`
 do
     echo "Levanto WebService con id $j - $(($j+1))/3"
-    gnome-terminal -- ./2-webservices.sh $j
+    gnome-terminal --title="WebService${j}" --geometry "100x20+0+$(($j*360))" -- ./2-webservices.sh $j
 done
 
 for i in `seq 0 $REPLICAS`
 do
     echo "Levanto Replica con id $i - $(($i+1))/$(($REPLICAS + 1))"
-    gnome-terminal -- ./1-alglobo.sh ./files/1.csv ./files/fallidos.csv $i
+    gnome-terminal --title="AlGlobo${i}" --geometry "100x20+960+$(($i*1080/($REPLICAS+1)))" -- ./1-alglobo.sh ./files/1.csv ./files/fallidos.csv $i
 done
