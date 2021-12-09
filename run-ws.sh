be9d5e74fa30d6217513204e6d280d554ba84ce5
guidobotta <gbotta@fi.uba.ar>
2021-12-09 00:59:24 -0300

    B

--- run-ws.sh
+++ run-ws.sh
@@ -12,9 +12,11 @@ rm alglobo/files/estado.log
 chmod +x 1-alglobo.sh
 chmod +x 2-webservices.sh
 
-gnome-terminal -- ./2-webservices.sh 0
-gnome-terminal -- ./2-webservices.sh 1
-gnome-terminal -- ./2-webservices.sh 2
+for j in `seq 0 2`
+do
+    echo "Levanto WebService con id $j - $(($j+1))/3"
+    gnome-terminal -- ./2-webservices.sh $j
+done
 
 for i in `seq 0 $REPLICAS`
 do
