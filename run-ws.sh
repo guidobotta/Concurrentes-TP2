19a27e0eabef79dc16a99b80b497f3c03c11733d
guidobotta <gbotta@fi.uba.ar>
2021-12-09 00:56:03 -0300

    A

--- run-ws.sh
+++ run-ws.sh
@@ -1,3 +1,14 @@
+REPLICAS=$(($1 - 1))
+
+if [ $# -eq 0 ]
+  then
+    echo "Usar como: ./run-ws.sh <num_rep>"
+    exit
+fi
+
+rm alglobo/files/fallidos.csv
+rm alglobo/files/estado.log
+
 chmod +x 1-alglobo.sh
 chmod +x 2-webservices.sh
 
@@ -5,5 +16,8 @@ gnome-terminal -- ./2-webservices.sh 0
 gnome-terminal -- ./2-webservices.sh 1
 gnome-terminal -- ./2-webservices.sh 2
 
-gnome-terminal -- ./1-alglobo.sh ./files/1.csv ./files/fallidos.csv 3
-gnome-terminal -- ./1-alglobo.sh ./files/1.csv ./files/fallidos.csv 4
+for i in `seq 0 $REPLICAS`
+do
+    echo "Levanto Replica con id $i - $(($i+1))/$(($REPLICAS + 1))"
+    gnome-terminal -- ./1-alglobo.sh ./files/1.csv ./files/fallidos.csv $i
+done
